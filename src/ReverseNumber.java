182c304eeb787ec364b60bd71615121f1eb31d00
adith <adith@adiths-Mac-mini.local>
2021-09-23 22:52:25 +0530

    first commit

---
+++ src/ReverseNumber.java
@@ -0,0 +1,21 @@
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		
+		// 1234 -> 4321
+		// 123 -> 321
+		
+		int num = 1234;
+		int rev =0; 
+		
+		while(num!=0) {
+			int n=num%10;   //1234%10 -> 4 is remainder
+			rev = rev*10+n; //0*10+4 =4
+			num = num/10;   //123
+		}
+			
+		System.out.println("reversed number is : " +rev);
+	}
+
+}
